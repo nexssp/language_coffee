3dd2463472e1020c10fbbfee84736a713bfaee65
Marcin Polak <mapoart@gmail.com>
2021-04-30 00:01:01 +0200

    removed deps of os

--- coffee.linux.nexss.config.js
+++ coffee.linux.nexss.config.js
@@ -1,9 +1,10 @@
 let languageConfig = Object.assign({}, require("./coffee.win32.nexss.config"));
-const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
-const sudo = os.sudo();
+const sudo = process.sudo;
 languageConfig.compilers = {
   coffee: {
-    install: os.replacePMByDistro(`${sudo}npm install --global coffeescript`),
+    install: process.replacePMByDistro(
+      `${sudo}npm install --global coffeescript`
+    ),
     command: "coffee",
     args: "<file>", //
     help: ``,
